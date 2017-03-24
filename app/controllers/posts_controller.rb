b3cadfd7e94c1bd5770ae99fa65a44a87f8b1f84
sihhan <thehappyend95@gmail.com>
2017-03-25 00:54:30 +0800

    action edit/delete in post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
-
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -20,7 +19,33 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
 
+  end
+  def update
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.user = current_user
+
+      if @post.update(post_params)
+    redirect_to account_posts_path, notice: "編輯成功"
+  else
+    render :edit
+  end
+    end
+
+    def destroy
+      @group = Group.find(params[:group_id])
+      @post = Post.find(params[:id])
+      @post.group = @group
+      @post.destroy
+      flash[:alert] = "Post deleted"
+      redirect_to account_posts_path
+    end
   private
 
   def post_params
